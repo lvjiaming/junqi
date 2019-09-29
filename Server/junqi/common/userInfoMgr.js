274fee096e981652ca92529b6028a0c8d25cfd00
lvjiaming <863537291@qq.com>
2019-09-29 11:08:05 +0800

    替换数据保存为mysql

--- Server/junqi/common/userInfoMgr.js
+++ Server/junqi/common/userInfoMgr.js
@@ -2,6 +2,8 @@
  * Created by Administrator on 2018/12/14.
  */
 const fs = require("fs");
+const mysql = require("../mysql");
+mysql.connect();
 module.exports = userInfoMgr = {
 
 };
@@ -9,28 +11,8 @@ module.exports = userInfoMgr = {
  *  增加一个玩家
  */
 userInfoMgr.insertUser = (user, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            dataObj[user.id] = user;
-            fs.writeFile("./common/userInfo.json", JSON.stringify(dataObj), (err) => {
-                if (err) {
-                    console.error("写入失败",err);
-                } else {
-                    console.log("写入成功");
-                }
-            });
-            console.log("新增一个玩家", user);
-            if (cb && cb instanceof Function) {
-                cb(dataObj);
-            }
-        }
+    mysql.insertUser(user, () => {
+        mysql.getAllUser(cb);
     });
 };
 /**
@@ -38,59 +20,14 @@ userInfoMgr.insertUser = (user, cb) => {
  * @param user
  */
 userInfoMgr.removeUser = (user, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            console.log(dataObj);
-            for (let us in dataObj) {
-                if (us && us == user) {
-                    delete dataObj[us];
-                }
-            }
-            fs.writeFile("./common/userInfo.json", JSON.stringify(dataObj), (err) => {
-                if (err) {
-                    console.error("写入失败",err);
-                } else {
-                    console.log("写入成功");
-                }
-            });
-            if (cb && cb instanceof Function) {
-                cb(dataObj);
-            }
-        }
-    });
+    mysql.delUserById(user.id);
 };
 /**
  *  获取一个玩家
  * @param id
  */
 userInfoMgr.getUserByName = (name, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            let getUser = null;
-            for (let us in dataObj) {
-                if (dataObj[us] && dataObj[us].name == name) {
-                    getUser = dataObj[us];
-                }
-            }
-            if (cb && cb instanceof Function) {
-                cb(getUser);
-            }
-        }
-    });
+    mysql.getUserByName(name, cb);
 };
 /**
  *  通过sessionId获取用户
@@ -98,26 +35,7 @@ userInfoMgr.getUserByName = (name, cb) => {
  * @param cb
  */
 userInfoMgr.getUserBySessionId = (id, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            let getUser = null;
-            for (let us in dataObj) {
-                if (dataObj[us] && dataObj[us].id == id) {
-                    getUser = dataObj[us];
-                }
-            }
-            if (cb && cb instanceof Function) {
-                cb(getUser);
-            }
-        }
-    });
+    mysql.getUserById(id, cb);
 };
 /**
  *  通过userId获取用户
@@ -125,76 +43,19 @@ userInfoMgr.getUserBySessionId = (id, cb) => {
  * @param cb
  */
 userInfoMgr.getUserByUserId = (id, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            let getUser = null;
-            for (let us in dataObj) {
-                if (dataObj[us] && dataObj[us].id == id) {
-                    getUser = dataObj[us];
-                }
-            }
-            if (cb && cb instanceof Function) {
-                cb(getUser);
-            }
-        }
-    });
+    mysql.getUserById(id, cb);
 };
 /**
  *  修改一个玩家
  * @param user
  */
 userInfoMgr.changeUser = (user, cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            for (let us in dataObj) {
-                if (us && us == user.id) {
-                    dataObj[us] = user;
-                }
-            }
-            fs.writeFile("./common/userInfo.json", JSON.stringify(dataObj), (err) => {
-                if (err) {
-                    console.error("写入失败",err);
-                } else {
-                    console.log("写入成功");
-                }
-            });
-            if (cb && cb instanceof Function) {
-                cb(dataObj);
-            }
-        }
-    });
+    mysql.updateUserInfo(user, cb);
 };
 /**
  * 得到用户列表
  * @param cb
  */
 userInfoMgr.getUserList = (cb) => {
-    fs.readFile("./common/userInfo.json", (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const dataStr = data.toString();
-            let dataObj = {};
-            if (dataStr) {
-                dataObj = JSON.parse(dataStr);
-            }
-            if (cb && cb instanceof Function) {
-                cb(dataObj);
-            }
-        }
-    });
+    mysql.getAllUser(cb);
 };
--- Server/junqi/mysql.js
+++ Server/junqi/mysql.js
@@ -48,15 +48,23 @@ module.exports = mySqlMgr = {
     /**
      *  插入玩家
      */
-    insertUser: (userInfo) => {
-        const sqlStr = `insert into user(id, name, password, online) values(${userInfo.id}, '${userInfo.name}', '${userInfo.password}', ${userInfo.online})`;
+    insertUser: (userInfo, cb) => {
+        const sqlStr = `insert into user(id, name, password, online) values(${userInfo.id}, '${userInfo.name}', 
+        '${userInfo.password}', ${userInfo.online ? 1 : 0})`;
         mySqlMgr.dealWithQuery(sqlStr, (result) => {
             if (result) {
                 console.log("插入成功！");
+                if (cb && cb instanceof Function) {
+                    cb(result);
+                }
             }
         });
     },
 
+    /**
+     *  通过id删除玩家
+     * @param id
+     */
     delUserById: (id) => {
         const sqlStr = `delete from user where id = '${id}'`;
         mySqlMgr.dealWithQuery(sqlStr, (result) => {
@@ -64,11 +72,75 @@ module.exports = mySqlMgr = {
                 console.log("删除成功！");
             }
         });
-    }
+    },
 
+    /**
+     *  获取玩家，通过id
+     * @param id
+     * @param cb
+     */
+    getUserById: (id, cb) => {
+        const sqlStr = `select * from user where id = '${id}'`;
+        mySqlMgr.dealWithQuery(sqlStr, (result) => {
+            if (result) {
+                if (cb && cb instanceof Function) {
+                    cb(result[0]);
+                }
+            }
+        });
+    },
 
+    /**
+     *  通过名字获取玩家
+     * @param name
+     * @param cb
+     */
+    getUserByName: (name, cb) => {
+        const sqlStr = `select * from user where name = '${name}'`;
+        mySqlMgr.dealWithQuery(sqlStr, (result) => {
+            if (result) {
+                // console.log("查找到的玩家！", result);
+                if (cb && cb instanceof Function) {
+                    cb(result[0]);
+                }
+            }
+        });
+    },
+
+    /**
+     *  修改玩家信息
+     */
+    updateUserInfo: (user, cb) => {
+        const sqlStr = `update user set id = '${user.id}', name = '${user.name}', 
+        password = '${user.password}', online = '${user.online ? 1 : 0}' where id = '${user.id}'`;
+        mySqlMgr.dealWithQuery(sqlStr, (result) => {
+            if (result) {
+                if (cb && cb instanceof Function) {
+                    cb(result);
+                }
+            }
+        });
+    },
+
+    /**
+     *  获取所有玩家
+     */
+    getAllUser: (cb) => {
+        const sqlStr = `select * from user`;
+        mySqlMgr.dealWithQuery(sqlStr, (result) => {
+            if (result) {
+                if (cb && cb instanceof Function) {
+                    cb(result);
+                }
+            }
+        });
+    },
 
 };
-mySqlMgr.connect();
-// mySqlMgr.insertUser({id: 123456, name: "user2", password: "12334", online: 1});
-mySqlMgr.delUserById(123456);
+// mySqlMgr.connect();
+// mySqlMgr.insertUser({id: 123457, name: "user1", password: "12334", online: 1});
+// mySqlMgr.delUserById(123456);
+// mySqlMgr.updateUserInfo({id: 123456, name: "user2", password: "12334", online: 0});
+// mySqlMgr.getUserByName("user1", (userlist) => {
+//     console.log("查询到:", userlist);
+// });
